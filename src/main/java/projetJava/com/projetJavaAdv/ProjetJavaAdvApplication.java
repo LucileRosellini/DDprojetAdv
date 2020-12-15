6e6481b987f2a93631fd45ec08fcb3f5ae247ec6
Lucile ROSELLINI <l.rosellini@cerberis.com>
2020-12-15 13:45:08 +0100

    v1 Thymleaf

---
+++ src/main/java/projetJava/com/projetJavaAdv/ProjetJavaAdvApplication.java
@@ -0,0 +1,13 @@
+package projetJava.com.projetJavaAdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaAdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetJava/com/projetJavaAdv/ProjetJavaAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package projetJava.com.projetJavaAdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
